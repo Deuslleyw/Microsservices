9e65c64d73ec7f09a2716e8c7d1a6a581f93d78e
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-09-20 02:34:09 -0300

    initialCc

---
+++ GrApp/src/test/java/com/ms/GrApp/GrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.GrApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
